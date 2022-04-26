c01462b61e92f61d8a7d6f547f802bbb20a9b69c
axl69 <96435788+axl69@users.noreply.github.com>
2022-04-26 12:38:45 +0200

    Add files via upload

---
+++ Tasca7/nivel1/Ex1n1/Trabajador.java
@@ -0,0 +1,68 @@
+package Ex1n1;
+
+public class Trabajador {
+
+	private String Nombre, Apellido;
+	protected int precioHora;
+
+	
+	public Trabajador(){
+		
+	}
+	
+	
+	public Trabajador(String Nombre, String Apellido ) {
+		this.Nombre = Nombre;
+		this.Apellido = Apellido;
+		
+		
+	}
+
+	public int getPrecioHora() {
+		return precioHora;
+	}
+
+	public void setPrecioHora(int precioHora) {
+		this.precioHora = precioHora;
+	}
+
+	
+
+	
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public String getApellido() {
+		return Apellido;
+	}
+
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	}
+
+	public int getSueldoHora() {
+		return precioHora;
+	}
+
+	public void setSueldoHora(int sueldoHora) {
+		this.precioHora = precioHora;
+	}
+
+	public void getSueldo(int d,int c) {
+
+	}
+
+	@Override
+	public String toString() {
+		return "Trabajador [Nombre=" + Nombre + ", Apellido=" + Apellido  ;
+	}
+	
+	
+
+}
